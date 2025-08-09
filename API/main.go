6a2ac0dd647d50c210c036167b02c733f8b392e2
Arvind <arvindsingh.2021cs@technonjr>
2025-08-09 23:12:46 +0530

    Http methods

--- API/main.go
+++ API/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"errors"
 	"net/http"
+
 	"github.com/gin-gonic/gin"
-	//"error"
 )
 
 type book struct{
@@ -21,6 +22,23 @@ var books=[]book{
 func getBooks(c *gin.Context){
 	c.IndentedJSON(http.StatusOK,books)
 }
+func bookById(c *gin.Context){
+	id:=c.Param("id")
+	book,err:=getBooksById(id)
+	if err!=nil{
+		c.IndentedJSON(http.StatusNotFound,gin.H{"message":"book not found"})
+		return
+	}
+	c.IndentedJSON(http.StatusOK,book)
+}
+func getBooksById(id string)(*book,error){
+	for i,b:=range books{
+		if b.ID==id{
+			return &books[i],nil
+		}
+	}
+	return nil,errors.New("book not found")
+}
 func createBook(c *gin.Context){
 	var newBook book
 	if err := c.BindJSON(&newBook); err != nil {
@@ -32,6 +50,7 @@ func createBook(c *gin.Context){
 func main(){
 	router:=gin.Default()
 	router.GET("/books",getBooks)
+	router.GET("/books/:id",bookById)
 	router.POST("/books",createBook)
 	router.Run("localhost:8080")
 
